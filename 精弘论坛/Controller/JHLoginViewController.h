55c175bd80961d45783a37d2ccf227bdd279ea4d
Dikey <Dikey@Dikey-2.local>
2014-08-28 08:46:01 +0800

    初始化

---
+++ 精弘论坛/Controller/JHLoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  JHLoginViewController.h
+//  精弘论坛
+//
+//  Created by Dikey on 8/27/14.
+//  Copyright (c) 2014 dikey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHLoginViewController : UIViewController<UITextFieldDelegate>
+
+
+@property (weak, nonatomic) IBOutlet UITextField *userName;
+@property (weak, nonatomic) IBOutlet UITextField *userPassword;
+
+@end
---
+++ 精弘论坛/Controller/JHLoginViewController.m
@@ -0,0 +1,67 @@
+//
+//  JHLoginViewController.m
+//  精弘论坛
+//
+//  Created by Dikey on 8/27/14.
+//  Copyright (c) 2014 dikey. All rights reserved.
+//
+
+#import "JHLoginViewController.h"
+
+@interface JHLoginViewController ()
+
+@end
+
+@implementation JHLoginViewController
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _userName.delegate =self;
+    _userPassword.delegate =self;
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+//开始登录
+-(void)startLoginProgress
+{
+    
+}
+
+//登录按钮
+- (IBAction)startLogin:(id)sender
+{
+    
+}
+
+//键盘切换
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if (textField == _userName) {
+        [_userPassword becomeFirstResponder];
+    }
+    if (textField == _userPassword) {
+        [_userPassword resignFirstResponder];
+    }
+    [self startLoginProgress];
+    //直接登录
+
+    
+    return YES;
+}
+
+
+//点击背景
+- (IBAction)backGroundTapped:(id)sender
+{
+    [_userPassword resignFirstResponder];
+    [_userName resignFirstResponder];
+}
+
+
+@end
