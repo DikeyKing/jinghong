3bcc541e4f1b9947a8a4da97a79e3a04798d8c9c
Dikey <Dikey@Dikey-2.local>
2014-10-10 15:47:18 +0800

    Todo：
     1.1 下拉刷新，有网络的时候测试 上拉加载更多：
     1.2 用固化实现缓存（固化所有论坛列表），后续缓存帖子列表
     1.3 发新帖子，发回复，是怎么传的数据？更复杂的语音，图片功能如何实现？
     1.4 左边和右边界面

--- 精弘论坛/JHAppDelegate.h
+++ 精弘论坛/JHAppDelegate.h
@@ -5,6 +5,9 @@
 //  Created by Dikey on 8/27/14.
 //  Copyright (c) 2014 dikey. All rights reserved.
 //
+//
+//
+
 
 #import <UIKit/UIKit.h>
 
@@ -13,3 +16,13 @@
 @property (strong, nonatomic) UIWindow *window;
 
 @end
+
+
+/*
+ Todo：
+ 
+ 1.1 下拉刷新，有网络的时候测试 上拉加载更多：
+ 1.2 用固化实现缓存（固化所有论坛列表），后续缓存帖子列表
+ 1.3 发新帖子，发回复，是怎么传的数据？更复杂的语音，图片功能如何实现？
+ 1.4 左边和右边界面
+*/
