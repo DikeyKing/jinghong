031b35b596097bd0249ac18c7daf187748448cf1
dikeyking <dikeyking@gmail.com>
2014-10-22 16:27:05 +0800

    缓存成功
    1 缓存到内存成功->缓存到硬盘失败
    
    2 下拉刷新重新获取，测试成功。
    3 上拉刷新加载更多，测试成功
    
    Todo：
    1 收到内存警告缓存到硬盘未完成（出错）
    2  上拉刷新加载更多（新内容）
    3 左、右界面功能构思

--- 精弘论坛/Modal/JHCache.swift
+++ 精弘论坛/Modal/JHCache.swift
@@ -78,28 +78,15 @@ import Foundation
         //假如内存中没有数据，从闪存中读取
         let path = self.cacheDirectory(fileName)
 
-//        if let dataFormCache:AnyObject = NSData.dataWithContentsOfFile(path, options: NSDataReadingOptions.DataReadingMappedIfSafe, error: nil){
-//            let unarchivedData: AnyObject = NSKeyedUnarchiver.unarchiveObjectWithData(dataFormCache as NSData)!
+        //path	String	"/var/mobile/Containers/Data/Application/6494B6BE-4C4C-4F3B-8BD5-6759DB8DAA9B/Library/CachesTopicAuthorCache1628780"	
+        
+        if let dataFormCache:AnyObject = NSData(contentsOfFile: path, options: NSDataReadingOptions.DataReadingMappedIfSafe, error: nil){
+            let unarchivedData: AnyObject = NSKeyedUnarchiver.unarchiveObjectWithData(dataFormCache as NSData)!
 //            cachedItemArray = unarchivedData as NSArray
 //            self.saveDataToMemory(dataFormCache)
 //            return unarchivedData as NSArray
-//        }
-        
-//        dataWithContentsOfFile(_:options:error:)' is unavailable:
-//        use  'NSData(contentsOfFile:options:error:)'
-        
-
-        //path	String	"/var/mobile/Containers/Data/Application/6494B6BE-4C4C-4F3B-8BD5-6759DB8DAA9B/Library/CachesTopicAuthorCache1628780"	
-        
-        if let dataFormCache:AnyObject = NSData(contentsOfFile: path, options: NSDataReadingOptions.DataReadingMappedIfSafe, error: nil)        {
-            let unarchivedData: AnyObject = NSKeyedUnarchiver.unarchiveObjectWithData(dataFormCache as NSData)!
-            cachedItemArray = unarchivedData as NSArray
-            self.saveDataToMemory(dataFormCache)
-            return unarchivedData as NSArray
         }
 
-        
-        
         return cachedItemArray
     }
     
@@ -111,13 +98,16 @@ import Foundation
     //私有方法，实现方法
     func receivedMemoryWarningAndSaveDataToDisk(notification: NSNotification){
         
-        self.memoryCache.removeAllObjects()
         //将所有数据缓存到Disk,并且清空缓存
         //遍历 self.memoryCache 中allKeys ，保存
 
         for (key, value) in self.memoryCache {
             self.saveMemoryCacheToDisk(value as NSData, fileName: key as String)
         }
+        
+        self.memoryCache.removeAllObjects()
+
+        
         println("收到了内存警告")
     }
     
